a105fb3c10da72742e85c51e64077aee2f9c6fab
AndikaDermawanSaputra <109196283+AndikaDermawanSaputra@users.noreply.github.com>
2024-12-07 01:56:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,39 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const healthRoutes = require('./routes/health');
-const { uploadFile } = require('./services/storageService');
-const { addUserData } = require('./services/firestoreService');
+const { addUserData } = require('./firestoreService');
+const admin = require('firebase-admin');
 
 
+// Inisialisasi Firebase app
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+} else {
+  admin.app(); // Gunakan app yang sudah ada
+}
+
+// Sekarang kamu bisa mengimpor firestoreService.js atau langsung menggunakan Firestore di sini
+const db = admin.firestore();
 const app = express();
 app.use(bodyParser.json());
 
-const projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;  // Mengambil Project ID
+const project = process.env.GOOGLE_CLOUD_PROJECT_ID;  // Mengambil Project ID
 const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME; // Mengambil Bucket Name
 
-console.log('Project ID:', projectId);
-console.log('Bucket Name:', bucketName);
+console.log('capstone-project-442701:', project);
+console.log('capstone-project-442701.appspot.com:', bucketName);
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-app.use('/health', healthRoutes);
+// Middleware untuk parsing JSON
+app.use(express.json());  // Pastikan body request bisa diparse dalam format JSON
+// Menggunakan rute untuk autentikasi (register, login)
+app.use('/api/auth', authRoutes);
+// Menggunakan rute untuk pengguna (save, edit data)
+app.use('/api/user', userRoutes);
+// Menggunakan rute untuk health (diagnose, recommendations)
+app.use('/api/health', healthRoutes);
 
 // Error handler
 app.use((err, req, res, next) => {
@@ -31,4 +47,6 @@ app.use((err, req, res, next) => {
 // Server
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 
