56bb74af25a27647efbf2d468ea134704a5d14ff
AndikaDermawanSaputra <109196283+AndikaDermawanSaputra@users.noreply.github.com>
2024-12-07 01:55:08 +0700

    Update firestore Service.js

--- service/firestore Service.js
+++ service/firestore Service.js
@@ -1,21 +1,25 @@
-// services/firestoreService.js
 const admin = require('firebase-admin');
-require('dotenv').config();
+require('dotenv').config();   // Pastikan .env file sudah benar
+const serviceAccount = require(process.env.FIREBASE_CREDENTIAL_PATH); 
 
-// Menginisialisasi Firebase Admin SDK dengan kredensial Firestore
-admin.initializeApp({
-  credential: admin.credential.cert(process.env.FIREBASE_PRIVATE_KEY_PATH),  // Path ke file kredensial
-  storageBucket: process.env.FIREBASE_BUCKET_NAME,
-});
+// Inisialisasi Firebase app
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+} else {
+  admin.app(); // Gunakan app yang sudah ada jika sudah diinisialisasi
+}
 
-// Mendapatkan referensi ke Firestore
+// Sekarang kamu bisa menggunakan Firestore atau layanan Firebase lainnya
 const db = admin.firestore();
 
 // Fungsi untuk menambahkan data ke Firestore
+
 const addUserData = async (userData) => {
   const usersRef = db.collection('users');
   const docRef = await usersRef.add(userData);
+  console.log('Document added with ID:', docRef.id);  // Pastikan id didapatkan
   return docRef.id;
 };
 
-module.exports = { addUserData };
